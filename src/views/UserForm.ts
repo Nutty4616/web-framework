80f91e38e96a30bcacff9691e1cee1d38545ba56
Nutty Nkosi <nkosin361@gmail.com>
2021-01-26 16:16:06 +0200

    Collection view and Users View added🚀

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,29 +1,45 @@
-export class UserForm {
-  constructor(public parent: Element) {}
+import { User, UserProps } from '../models/User';
+import { View } from '../views/View';
 
-  eventsMap():{ [key: string]: () => void } {
+type method = () => void;
+
+export class UserForm extends View<User, UserProps> {
+  eventsMap():{ [key: string]: method } {
     return {
-      'click:button': this.onButtonClick
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
     };
   }
 
-  onButtonClick(): void {
-    console.log('Hi there');
+  onSaveClick = (): void => {
+    this.model.save();
+  }
+
+   onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  }
+
+   onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+
+    if(input) {
+      const name = input.value;
+
+      this.model.set({ name });
+    }  
   }
 
   template(): string{
     return `
       <div>
-        <h1>User Form</h1>
-        <input />  
+        <input placeholder="${this.model.get('name')}" />  
+        <button class="set-name">Change Name</button>
+        <button class="set-age">Set Random Age</button>
+        <button class="save-model">Save User</button>
       </div>
     `;
   }
 
-  render(): void {
-    const templateElement = document.createElement('template');
-    templateElement.innerHTML = this.template();
-
-    this.parent.append(templateElement.content);
-  }
+  
 }
